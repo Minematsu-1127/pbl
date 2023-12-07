290df431b86104ddfa867b17529b5ba9f4a5c301
みね <149132530+Minematsu-1127@users.noreply.github.com>
2023-12-07 14:54:55 +0900

    Update samlpe.py

--- samlpe.py
+++ samlpe.py
@@ -16,18 +16,19 @@ def main():
 
     st.markdown("# &#8203;``【忙しい方必見！】``&#8203;\n# もぎたてテレビを簡単まとめ")
 
-    # st.image("thum_mogitate.png", width=500)
+    st.image("thum_mogitate.png", width=500)
 
     # チャット履歴の初期化
     if "messages" not in st.session_state:
         st.session_state.messages = [  ]
 
-   # テキスト入力またはファイルアップロードの選択
+      # テキスト入力またはファイルアップロードの選択
     option = st.radio("テキスト入力またはファイルアップロード", ("テキスト入力", "ファイルアップロード"))
 
     if option == "テキスト入力":
         # テキスト入力
-        if user_input := st.text_area("もぎたてテレビの原稿を入力してください", ""):
+        user_input = st.text_area("もぎたてテレビの原稿を入力してください", "")
+        if st.button("要約する"):
             st.session_state.messages.append(HumanMessage(content=user_input))
             with st.spinner("ChatGPT is typing ..."):
                 response = llm(st.session_state.messages)
@@ -38,10 +39,11 @@ def main():
         if uploaded_file is not None:
             file_contents = uploaded_file.read()
             input_text = file_contents.decode("utf-8")
-            st.session_state.messages.append(HumanMessage(content=input_text))
-            with st.spinner("ChatGPT is typing ..."):
-                response = llm(st.session_state.messages)
-            st.session_state.messages.append(AIMessage(content=response.content))
+            if st.button("要約する"):
+                st.session_state.messages.append(HumanMessage(content=input_text))
+                with st.spinner("ChatGPT is typing ..."):
+                    response = llm(st.session_state.messages)
+                st.session_state.messages.append(AIMessage(content=response.content))
 
 
     # チャット履歴の表示
